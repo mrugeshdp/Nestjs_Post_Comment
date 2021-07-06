ceeb757cc4e21b0d246fcd6afd5ae9ffb8ebe76a
Mrugesh Patel <mdp@Mrugeshs-MacBook-Air.local>
2021-07-06 10:38:05 +0530

    Rollback aws changes

--- src/db-connection/pg.config.ts
+++ src/db-connection/pg.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: 'nestjspostcomment.caqvgoqygzpq.ap-south-1.rds.amazonaws.com',
+  host: 'localhost',
   port: 5432,
   username: 'postgres',
   password: 'postgres',
